a3f1eab8eb86bd23bd25cbf2abbf0d1765270a7a
unclepis <59869158@qq.com>
2017-08-03 13:43:37 +0800

    Update About.js
    增加了context.router.push(some/path)的方式完成点击路由跳转

--- modules/About.js
+++ modules/About.js
@@ -1,8 +1,28 @@
 import React from 'react';
 export default React.createClass({
+    contextTypes: {
+        router: React.PropTypes.object
+    },
+    submit: function (e) {
+        e.preventDefault();
+        const path = e.target.elements[0].value;
+        const password = e.target.elements[1].value;
+        if (password === '000') {
+            this.context.router.push(`/${path}`);
+        } else {
+            alert('password error');
+        }
+    },
     render() {
         return (
-            <h1>This is About single Page!</h1 >
+            <div>
+                <h1>This is New About single Page!</h1 >
+                <form onSubmit={this.submit}>
+                    <input type='text' placeholder='path' /><br />
+                    <input type='password' placeholder='passWord' /><br />
+                    <input type='submit' value='submit' />
+                </form>
+            </div>
         );
     }
 })
