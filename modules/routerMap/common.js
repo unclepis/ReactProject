76f3e957e08fe907862d534d994b727bb57b98d3
unclepis <59869158@qq.com>
2017-08-04 16:07:09 +0800

    Create common.js

---
+++ modules/routerMap/common.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import NavLink from './navLink'
+import { IndexLink } from 'react-router'
+export default class Common extends React.Component{
+    render() {
+        return (
+            <div>
+                <ul role="nav" className='navClass'>
+                    <li><IndexLink to="/" activeClassName='nav'>Home</IndexLink></li>
+                    <li><NavLink to="/about">About</NavLink></li>
+                    <li><NavLink to="/repos">Repos</NavLink></li>
+                    <li><NavLink to="/repos/reactjs/reactRouter">react router</NavLink></li>
+                    <li><NavLink to="/repos/facebook/react">react</NavLink></li>
+                </ul>
+                <h1>welcome to React Router Tutorial</h1>
+            </div >
+        )
+    }
+}
