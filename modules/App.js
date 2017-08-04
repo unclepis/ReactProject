39b500a607f3c3daddf77009de64165c31fa9ec5
unclepis <59869158@qq.com>
2017-08-04 16:07:45 +0800

    Update App.js

--- modules/App.js
+++ modules/App.js
@@ -1,18 +1,10 @@
 import React from 'react'
-import NavLink from './routerMap/navLink'
-import { IndexLink } from 'react-router'
+import Common from './routerMap/common'
 export default React.createClass({
   render() {
     return (
       <div>
-        <ul role="nav" className='navClass'>
-          <li><IndexLink to="/" activeClassName='nav'>Home</IndexLink></li>
-          <li><NavLink to="/about">About</NavLink></li>
-          <li><NavLink to="/repos">Repos</NavLink></li>
-          <li><NavLink to="/repos/reactjs/reactRouter">react router</NavLink></li>
-          <li><NavLink to="/repos/facebook/react">react</NavLink></li>
-        </ul>
-        <h1>React Router Tutorial</h1>
+        <Common />
         {this.props.children || <Home />}
       </div >
     )
