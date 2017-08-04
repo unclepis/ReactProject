f71e64a2d71288deebe6121e02ddd877a975ad47
unclepis <59869158@qq.com>
2017-08-04 16:29:39 +0800

    Update common.js
    增加局部变量的引用style.nav

--- modules/routerMap/common.js
+++ modules/routerMap/common.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import NavLink from './navLink'
+import styles from '../../css/index.css'
 import { IndexLink } from 'react-router'
 export default class Common extends React.Component{
     render() {
         return (
             <div>
                 <ul role="nav" className='navClass'>
-                    <li><IndexLink to="/" activeClassName='nav'>Home</IndexLink></li>
+                    <li><IndexLink to="/" activeClassName={styles.nav}>Home</IndexLink></li>
                     <li><NavLink to="/about">About</NavLink></li>
                     <li><NavLink to="/repos">Repos</NavLink></li>
                     <li><NavLink to="/repos/reactjs/reactRouter">react router</NavLink></li>
