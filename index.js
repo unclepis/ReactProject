4791bb28b122e6d33598f658dfdf655101fa262e
unclepis <59869158@qq.com>
2017-08-04 16:03:37 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import React from 'react'
 import Routes from './modules/routerMap/routes'
 import { render } from 'react-dom';
+import './css/index.css'
 render(<Routes />, document.getElementById('app'))
